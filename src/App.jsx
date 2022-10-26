08e56c261b14390de901809d40289f20e97bd751
AgustinPalmaM <palma.agustin7@gmail.com>
2022-10-25 23:58:08 -0300

    Create main structure with buttons for numbers and operators

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='title-container'>
+        <h1 className='title'>Online Calculator</h1>
+      </div>
+      <div className="calculator-container">
+        <div className='fila'>
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className='fila'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+        <div className='fila'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>*</Button>
+        </div>
+        <div className='fila'>
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>
+        </div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
